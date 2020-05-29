bc596cf7dccb5c955082648315d7bf469ba0e1c4
Chris Friemann <50674927+Chrinsieboy@users.noreply.github.com>
2020-05-29 10:16:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -65,12 +65,7 @@ client.on("message", async message => {
     return message.channel.send(serverEmbed);
     }
 
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
-
-	if (command === 'stats') {
+	if (command === `${prefix}stats`) {
 		return message.channel.send(`Server count: ${client.guilds.cache.size}`);
     }
     
