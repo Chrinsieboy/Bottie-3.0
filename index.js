156c71e7b95761f8fbe03c7cff455d0708cbbaba
Chris Friemann <50674927+Chrinsieboy@users.noreply.github.com>
2020-06-13 11:27:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ client.on("message", async message => {
             .setDescription("Hier zie je de info van de bot")
             .setColor("#ff0000")
             .addField("Naam bot", client.user.username)
+            .addField("Gemaakt op", client.user.createdAt)
+            .addField("Gemaakt door", "Chrinsieboyyy#3976")
+            .addField("ID:", bot.user.id)
 
- 
             .setThumbnail('https://www.chris.friemann.nl/discord/logo.png')
             .setTimestamp()
             .setFooter('© 2020 Bottie 3.0. Alle Rechten Voorbehouden.', 'https://www.chris.friemann.nl/discord/logo.png');
